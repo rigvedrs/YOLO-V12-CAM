e9bb688818c6a2a0a137b1062a30b8df40c55c9f
mahotilo <kvmahotilo@gmail.com>
2025-06-03 21:51:50 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,6 @@ setup(
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'torch',
-        'opencv-python',
-        'numpy',
-        'matplotlib'
+        'ttach'
     ],
 )
